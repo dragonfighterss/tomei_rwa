576f434ab91c80f84101b89334c606378a36e687
dragonfighterss <dragonfighter0111@outlook.com>
2023-09-13 14:47:44 -0400

    fix-> import external font style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import './assets/fonts/Rand-Light/fonts.css';
-// import './assets/fonts/Rand-Medium/fonts.css';
-// import './assets/fonts/Rand-Thin/fonts.css';
-// import './assets/fonts/TTF/RapidST_Regular.ttf'; 
+import './assets/fonts/Rand-Light/fonts.css';
+import './assets/fonts/Rand-Medium/fonts.css';
+import './assets/fonts/Rand-Thin/fonts.css';
+import './assets/fonts/TTF/RapidST_Regular.ttf'; 
 import './assets/fonts/WOFF/RapidST_Regular.woff'; 
-// import './assets/fonts/TTF/RapidST_Regular.woff2'; 
+import './assets/fonts/WOFF2/RapidST_Regular.woff2'; 
+import './assets/fonts/OTF/RapidST_Regular.otf'; 
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
