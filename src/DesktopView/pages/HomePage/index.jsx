4ba9e57566849d1852a7b9c9e130f49399ee4d51
dragonfighterss <dragonfighter0111@outlook.com>
2023-09-15 11:26:50 -0400

    complete feedback push again

--- src/DesktopView/pages/HomePage/index.jsx
+++ src/DesktopView/pages/HomePage/index.jsx
@@ -6,7 +6,7 @@ import Section4 from "./Section4";
 import Section5 from "./Section5";
 import Section6 from "./Section6";
 import Section7 from "./Section7";
-
+// 
 const Homepage = (props) => {
   return (
     <div>
